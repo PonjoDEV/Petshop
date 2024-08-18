2965ff6c5d38b70b03647b0a64f530c5f3f171a7
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2024-08-18 10:56:17 -0300

    build: modifications to let the infrastructure more light

---
+++ client/docker/start-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "---------------------------------------------"
+echo "      Starting Client From Docker..."
+echo "---------------------------------------------"
+gradle bootRun
---
+++ cloud-config/docker/start-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "---------------------------------------------"
+echo "      Starting Cloud Config Sever From docker..."
+echo "---------------------------------------------"
+gradle bootRun
